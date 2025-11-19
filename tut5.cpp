9c474a2b9085aca9dbdd06c121931f5c89cf9150
Ayush Agarwal <ayushagarwal191106@gmail.com>
2025-11-19 23:21:00 +0530

    remove comments

--- tut5.cpp
+++ tut5.cpp
@@ -4,12 +4,9 @@ using namespace std;
 int main()
 {
     int num1 , num2;
-    cout<<"Enter the value of num1:\n"; /* '<<' This is called Insertion operator */
-    cin>> num1; /* '>>' This is called Extraction operator */
-
-    cout<<"Enter the value of num2:\n"; /* '<<' This is called Insertion operator */
-    cin>> num2; /* '>>' This is called Extraction operator */
-
+    cout<<"Enter the value of num1:\n"; 
+    cout<<"Enter the value of num2:\n"; 
+    cin>> num2; 
     cout<<"The sum is "<< num1+num2;
     return 0;
 }
