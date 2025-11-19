5b6090dc3a881355cb4bd70e396bd3a7fc17dd70
AYUSH AGARWAL <ayushagarwal@AYUSHs-MacBook-Air.local>
2025-11-19 22:02:52 +0530

    Initial commit

---
+++ Hello.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+
+int main() {
+    int a = 22;
+    int b = a;
+    int c = b * 6;
+    int d = 1 , e;
+    
+    int x,y,z;
+    x = y = z = 4;
+
+    int age = 2;
+    int years = 22;
+    int newage = age + years;
+}
